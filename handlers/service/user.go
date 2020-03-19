6f4bb0b941ac39ea0ae73e43f2c36afaa636fcaf
Mihai Oprea <mihai.oprea@gmail.com>
2020-03-19 06:44:49 +0100

    added a static user database

--- handlers/service/user.go
+++ handlers/service/user.go
@@ -1,9 +1,49 @@
 package service
 
-import "DockerHttpClient/data"
+import (
+	"DockerHttpClient/data"
+	"errors"
+)
+
+type user struct {
+	username string
+	password string
+	profile  data.User
+}
+
+var users []user
+
+func init() {
+	users = make([]user, 0)
+	users = append(users, user{
+		username: "mihai",
+		password: "password",
+		profile: data.User{
+			Username: "mihai",
+			Email:    "mihai@devops.org",
+			Role:     "admin",
+		},
+	})
+
+	users = append(users, user{
+		username: "user1",
+		password: "userpass",
+		profile: data.User{
+			Username: "user1",
+			Email:    "user1@devops.org",
+			Role:     "user",
+		},
+	})
+}
 
 // Login checks if the credentials are valid
-func Login(username string, passwd string) (data.User, error) {
+func Login(username string, passwd string) (*data.User, error) {
 
-	return data.User{Username: username, Role: "admin"}, nil
+	for _, u := range users {
+		if username == u.username && passwd == u.password {
+			return &u.profile, nil
+		}
+	}
+
+	return nil, errors.New("Unknown user credentials")
 }
